f04f3a8817fc8d19cffbf33acbc972c628cfe343
root <josephmc5>
2012-11-03 18:26:05 -0400

    rework..getting closer

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,66 +1,46 @@
 class sabnzbd::config {
-    
-    $api_key = hiera("sabnzbd_api_key")
-    $email_to = hiera("email_to")
-    $email_account = hiera("email_from")
-    $email_server = hiera("email_server")
-    $email_from = hiera("email_from")
-    $email_passwd = hiera("email_passwd")
-    $nzb_key = hiera("sabnzbd_nzb_key")
-    $nzb_server_username = hiera("nzb_server_username")
-    $nzb_server = hiera("nzb_server")
-    $nzb_server_passwd = hiera("nzb_server_passwd")
-    $nzb_server_port = hiera("nzb_server_port")
-    $nzb_server_retention = hiera("nzb_server_retention")
-    $nzb_server_maxconnections = hiera("nzb_server_maxconnections")
-    $nzb_server_ssl = hiera("nzb_server_ssl")
-    if ($nzbmatrix_username) {
-        $nzbmatrix_username = hiera("nzbmatrix_username")
-        $nzbmatrix_apikey = hiera("nzbmatrix_apikey")
+    file { "$base_dir/sabnzbd/config/":
+        ensure => directory,
+        owner => 'sabnzbd',
+        group => 'sabnzbd',
     }
-    
-    $base_dir = hiera("app_dir")
-    if ($nzb_scan_dir) {
-        $nzb_scan_dir = hiera("nbz_scan_dir")
-    }
-    $complete_dir = hiera("complete_download_dir")
-    $downloads_dir = hiera("incomplete_download_dir")
-    
-    $src_dir = "src"    
-    $data_dir = "data"    
-    $scripts_dir = "$base_dir/sabnzbd/$data_dir/scripts"
-
-    file { "$base_dir/sabnzbd/$data_dir/sabnzbd.ini":
+    file { "$base_dir/sabnzbd/config/sabnzbd.ini":
         content => template('sabnzbd/sabnzbd.ini.erb'),
         owner => 'sabnzbd',
         group => 'sabnzbd',
         mode => '0644',
-        require => Exec['download-sabnzbd']
+        require => File["$base_dir/sabnzbd/config/"],
     }
     
-    file { "$base_dir/sabnzbd/scripts":
+    file { "$cache_dir":
         ensure => directory,
         owner => 'sabnzbd',
-        group => 'automators',
+        group => 'sabnzbd',
+        mode => '0644',
+    }
+    file { "$scripts_dir":
+        ensure => directory,
+        owner => 'sabnzbd',
+        group => 'sabnzbd',
         mode => '0644',
     }
 
-    file { "$base_dir/sabnzbd/scripts/autoProcessTV.py":
+    file { "$scripts_dir/autoProcessTV.py":
         ensure => link,
-        target => "$base_dir/sickbeard/autoProcessTV/autoProcessTV.py",
-        require => File["$base_dir/sabnzbd/scripts","$base_dir/sickbeard"]
+        target => "$base_dir/sickbeard/src/autoProcessTV/autoProcessTV.py",
+        require => Exec['download-sickbeard'],
     }
     
-    file { "$base_dir/scripts/autoProcessTV.cfg":
+    file { "$scripts_dir/autoProcessTV.cfg":
         ensure => link,
-        target => "/opt/sickbeard/autoProcessTV/autoProcessTV.cfg",
-        require => File["$base_dir/sabnzbd/scripts"]
+        target => "$base_dir/sickbeard/config/autoProcessTV.cfg",
+        require => File["$scripts_dir","$base_dir/sickbeard/config/autoProcessTV.cfg"]
     }
     
-    file { "$base_dir/scripts/sabToSickBeard.py":
+    file { "$scripts_dir/sabToSickBeard.py":
         ensure => link,
-        target => "/opt/sickbeard/autoProcessTV/sabToSickBeard.py",
-        require => File["$base_dir/sabnzbd/scripts"]
+        target => "$base_dir/sickbeard/src/autoProcessTV/sabToSickBeard.py",
+        require => Exec['download-sickbeard'],
     }
     
 }
---
+++ manifests/params.pp
@@ -0,0 +1,34 @@
+class sabnzbd::params (
+    $api_key = hiera("sabnzbd_api_key"),
+    $email_to = hiera("email_to"),
+    $email_account = hiera("email_from"),
+    $email_server = hiera("email_server"),
+    $email_from = hiera("email_from"),
+    $email_passwd = hiera("email_passwd"),
+    $nzb_key = hiera("sabnzbd_nzb_key"),
+    $ssl = hiera("sabnzbd_ssl", "0"),
+    $nzb_server_username = hiera("nzb_server_username"),
+    $nzb_server = hiera("nzb_server"),
+    $nzb_server_passwd = hiera("nzb_server_passwd"),
+    $nzb_server_port = hiera("nzb_server_port"),
+    $nzb_server_retention = hiera("nzb_server_retention"),
+    $nzb_server_maxconnections = hiera("nzb_server_maxconnections", "20"),
+    $nzb_server_ssl = hiera("nzb_server_ssl", "1"),
+    $nzbmatrix_username = hiera("nzbmatrix_username", ""),
+    $nzbmatrix_apikey = hiera("nzbmatrix_apikey", ""),
+    $nzb_scan_dir = hiera("nbz_scan_dir", ""),
+    $base_dir = hiera("app_dir", "/opt"),
+    $complete_dir = hiera("complete_download_dir"),
+    $downloads_dir = hiera("incomplete_download_dir"),
+    $complete_movie_download_dir = hiera("complete_movie_download_dir", "moveies"),
+    $complete_tv_download_dir = hiera("complete_tv_download_dir", "tv"),
+    $complete_music_download_dir = hiera("complete_music_download_dir", "music"),
+    $venv_dir = "venv",
+    $url = hiera("sabnzbd_git_url", "https://github.com/sabnzbd/sabnzbd.git")
+)
+{
+    $scripts_dir = "$base_dir/sabnzbd/scripts"
+    $log_dir = "$base_dir/sabnzbd/log"
+    $admin_dir = "$base_dir/sabnzbd/admin"
+    $cache_dir = "$base_dir/sabnzbd/cache"
+}
